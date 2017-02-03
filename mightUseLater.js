c13df75e1b160721573f4f0bcc98b783828ad655
Benjamin Earl <benearl72@gmail.com>
2017-02-03 15:37:15 -0700

    Threw instructor to old, cause unused

--- mightUseLater.js
+++ mightUseLater.js
@@ -72,3 +72,55 @@ function getAdminId(options,missingData,callback){
 		})
 	}
 }
+
+// Instructor Class
+
+class Instructor {
+	constructor(firstName,lastName,email){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		// Checking to see if proffessor has already been created
+		this.alreadyExists( err => {
+			if(err) { console.error(err); return }
+
+			// Create the Instructor
+			this.createInstructor( err => {
+				if(err) { console.error(err); return }
+
+				// Add Instructor to Instructor Group
+				this.joinGroup( err => { if(err) { console.error(err) } });
+
+
+			})
+		})
+	}
+	alreadyExists(callback){
+		session.sendRequest("principal-list&filter-email="+this.email,"principal-list.principal@principal-id", id =>{
+			if(id == undefined)
+				callback(null);
+			else
+				callback(this.firstName+" "+this.lastName+" has already been created")
+		})
+	}
+	createInstructor(callback){
+		var queryString = "principal-update&first-name="+this.firstName+"&last-name="+this.lastName+"&login="+this.email+"&password=byuitemp123&type=user&send-email=true&has-children=0&email="+this.email;
+		session.sendRequest(queryString,"principal@principal-id",id => {
+			this.id = id;
+			if(id == undefined)
+				callback("Error creating "+this.firstName+" "+this.lastName);
+			else
+				callback(null);
+		})
+	}
+	// TODO: somehow find where to get this id for the proffesor vvvv
+	joinGroup(callback){
+		var queryString = "group-membership-update&group-id="+1017936982+"&is-member=true&principal-id="+this.id;
+		session.sendRequest(queryString,"status@code", ok => {
+			if(ok == "ok")
+				callback(null)
+			else
+				callback("Error adding "+this.firstName+" "+this.lastName+" to instructor group")
+		})
+	}
+}
