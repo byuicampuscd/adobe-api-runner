7e232d37671fb18ccc31ec5df14ec43bf0e6cc7f
Benjamin Earl <benearl72@gmail.com>
2017-02-03 15:32:30 -0700

    Completley Rewrote the API runner

--- adobe-api-runner.js
+++ adobe-api-runner.js
@@ -2,45 +2,61 @@ var xmldoc = require('xmldoc');
 var https = require('https');
 var fs = require('fs');
 var cli = require('./cli');
+var dsv = require('d3-dsv');
+var asyn = require('async')
+var defaultNumOfMeetings = 10;
 
-class Session {
+class Main {
 	constructor(settings,callback){
 		this.settings = settings;
+
 		this.login(settings.loginInfo, ableToLogin => {
-			if(ableToLogin){
-				callback();
-				return
-			} else {
-				console.log("Unable to login");
-			}
+			if(!ableToLogin){ console.error("Unable to login"); return; }
+
+			this.parseFiles( err => {
+				if(err){console.error(err); return}
+				this.createEverything();
+			});
 		})
 	}
+
 	sendRequest(queryString,xmlPath,callback){
-		
-		var req = https.request({host:this.settings.domain+".adobeconnect.com",path: "/api/xml?action="+queryString, headers: { 'cookie' : this.cookie} }, res => {
-			var body = '';
+		let req = https.request({host:this.settings.domain+".adobeconnect.com",path: "/api/xml?action="+queryString, headers: { 'cookie' : this.cookie} }, res => {
+			let body = '';
 			res.on('data', chunk => { body += chunk})
 			res.on('end', () => {
-				if(xmlPath)
-					callback(new xmldoc.XmlDocument(body).valueWithPath(xmlPath));
-				else
-					callback(new xmldoc.XmlDocument(body));
+				let doc;
+				let failed = false;
+				try{
+					doc = new xmldoc.XmlDocument(body);
+				} catch (e) {
+					failed = true;
+					callback("Error: Overwellmed Server",undefined)
+				} finally {
+					if(!failed){
+						if(xmlPath)
+							callback(null,doc.valueWithPath(xmlPath));
+						else
+							callback(null,doc);
+					}
+				}
+
 			})
 		})
-		req.on('error', console.error);
+		req.on('error', callback);
 		req.end();
 	}
 	
 	login(loginInfo,callback){
-		var options = {
+		let options = {
 			host: this.settings.domain+".adobeconnect.com",
 			path: "/api/xml",
 			method: "POST",
 			headers: { 'Content-Type' : 'application/xml' }
 		}
-		var postData = "<params><param name='action'>login</param><param name='login'>"+loginInfo.username+"</param><param name='password'>"+loginInfo.password+"</param></params>"
+		let postData = "<params><param name='action'>login</param><param name='login'>"+loginInfo.username+"</param><param name='password'>"+loginInfo.password+"</param></params>"
 
-		var req = https.request(options, res => {
+		let req = https.request(options, res => {
 			this.cookie = res.headers['set-cookie'][0];
 			res.on('data', body => {
 				// checking to see if we were able to login
@@ -54,215 +70,213 @@ class Session {
 		req.write(postData);
 		req.end();
 	}
-
 	
-}
+	parseFiles(callback){
+		fs.readFile(this.settings.courseList,'utf8',(err,table) => {
+			if(err) { callback(err) }
 
-class Instructor {
-	constructor(firstName,lastName,email){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		// Checking to see if proffessor has already been created
-		this.alreadyExists( err => {
-			if(err) { console.error(err); return }
+			// Check to see if they have my headers
+			var numMeetLookUp =  dsv.csvParse(table)
+			if(numMeetLookUp.columns.indexOf("Course Name") == -1 ||
+			   numMeetLookUp.columns.indexOf("Number of Meetings") == -1){
+				console.error("Error: Your table headings aren't 'Course Name' and 'Number of Meetings'")
+				return;
+			}
 
-			// Create the Instructor
-			this.createInstructor( err => {
-				if(err) { console.error(err); return }
-				
-				// Add Instructor to Instructor Group
-				this.joinGroup( err => { if(err) { console.error(err) } });
-				
-				
-			})
-		})
-	}
-	alreadyExists(callback){
-		session.sendRequest("principal-list&filter-email="+this.email,"principal-list.principal@principal-id", id =>{ 
-			if(id == undefined)
-				callback(null);
-			else
-				callback(this.firstName+" "+this.lastName+" has already been created")
-		})
-	}
-	createInstructor(callback){
-		var queryString = "principal-update&first-name="+this.firstName+"&last-name="+this.lastName+"&login="+this.email+"&password=byuitemp123&type=user&send-email=true&has-children=0&email="+this.email;
-		session.sendRequest(queryString,"principal@principal-id",id => {
-			this.id = id;
-			if(id == undefined)
-				callback("Error creating "+this.firstName+" "+this.lastName);
-			else
-				callback(null);
-		})
-	}
-	// TODO: somehow find where to get this id for the proffesor vvvv
-	joinGroup(callback){
-		var queryString = "group-membership-update&group-id="+1017936982+"&is-member=true&principal-id="+this.id;
-		session.sendRequest(queryString,"status@code", ok => {
-			if(ok == "ok")
-				callback(null)
-			else
-				callback("Error adding "+this.firstName+" "+this.lastName+" to instructor group")
-		})
-	}
-}
+			// create the look up object from the courseList
+			numMeetLookUp = numMeetLookUp.reduce( (obj,row) => {
+				obj[row["Course Name"].replace(" ","_").toUpperCase()] = row["Number of Meetings"]
+				return obj
+			} ,{})
 
-class Class {
-	
-	constructor(courseName,sectionNum,numOfMeetings){
-		
-		// Conforming to Naming Conventions early on
-		this.numOfMeetings = numOfMeetings;
-		this.courseName = courseName.replace(" ","_").toUpperCase();
-		this.sectionNum = ("00"+sectionNum).slice(-2);
-		this.meetingName = this.courseName.replace("_","") + "_G0" + this.sectionNum + "_";
-		
-		// I have a bug where when a new course folder is created,
-		// the next calls to find that new folder fail, probably
-		// because the search is just getting a cached result
-		// so I am saving the ids of all the folders I create
-		this.savedCourseIDs = {};
+			fs.readFile(this.settings.sectionList,'utf8',(err,table) => {
+				if(err) { callback(err) }
 
-		// Creates the Course folder if not already created
-		this.getFolderID(session.settings.coursesFolderID,this.courseName, id => {
-
-			// This may be triggerd for the reason described above
-			if(id == undefined){
-				if(this.savedCourseIDs[this.courseName] != undefined)
-					this.courseID = savedCourseIDs[this.courseName];
-				else {
-					console.error("Error creating the course folder for "+this.courseName+" Check the courses folder ID in the settings file")
+				// Check to see if they have my headers
+				var data =  dsv.csvParse(table)
+				if(data.columns.indexOf("Course Name") == -1 ||
+				   data.columns.indexOf("Section") == -1 ||
+				   data.columns.indexOf("Number of Meetings") == -1){
+					console.error("Error: Your table headings aren't 'Course Name' , 'Section' , 'Number of Meetings'")
 					return;
 				}
-			}
-			this.savedCourseIDs[this.courseName] = id;
-			this.courseID = id;
-			
-			// Checking to see if Section already Exists
-			this.findFolderID(this.courseID,"SET_"+this.sectionNum, id => {
-				if(id){ 
-					// section already exists, so check to see if it needs more meetings
-					this.sectionID = id;
-					this.createMoreMeetings();
-				}
-				else {
-					this.createSection();
-				}
-			})
-		});
-	}
-	createMoreMeetings(){
-		this.getNumOfMeetings(this.sectionID, actualNum => {
-			if(actualNum == this.numOfMeetings){
-				console.error(this.courseName+"-"+this.sectionNum+" has already been created with "+this.numOfMeetings+" meetings");
-				return;
-			} else if(this.numOfMeetings < actualNum){
-				console.error(this.courseName+"-"+this.sectionNum+" has already been created with "+actualNum+" meetings, you only requested "+this.numOfMeetings);
-				return;
-			} else if(this.numOfMeetings > actualNum){
-				console.log("Creating "+(this.numOfMeetings-actualNum)+" additional meetings for "+this.courseName+"-"+this.sectionNum)
-				for(var i = actualNum+1; i <= this.numOfMeetings; i++){
-					this.meetingName += ("000"+i).slice(-3)
-					this.createMeeting();
-				}
-			}
-		})
-	}
-	createSection(){
-		this.createFolder(this.courseID,"SET_"+this.sectionNum, id => { 
-			if(id == undefined)
-				console.error("Error creating "+this.courseName+"-"+this.sectionNum)
-			else {
-				this.sectionID = id;
-				//  Now we finally get to create the meetings!
-				//	console.log("Creating the meetings for "+this.courseName+"-"+this.sectionNum)
-				for(var i = 1; i <= this.numOfMeetings; i++){
-					this.meetingName += ("000"+i).slice(-3)
-					this.createMeeting();
-				}
-			}
-		})
-	}
-	findFolderID(parentFolderID,folderName,callback){
-		session.sendRequest("sco-contents&sco-id="+parentFolderID+"&filter-name="+folderName,"scos.sco@sco-id", callback)
-	}
-	createFolder(parentFolderID,folderName,callback){
-		session.sendRequest("sco-update&folder-id="+parentFolderID+"&type=folder&name="+folderName,"sco@sco-id", callback)
-	}
-	// Creates the folder if not found
-	getFolderID(parentFolderID,folderName,callback){
-		this.findFolderID(parentFolderID,folderName, id => {
-			if(id)
-				callback(id);
-			else {
-				this.createFolder(parentFolderID,folderName, callback)
-			}
-		})
-	}
-	getNumOfMeetings(folderID,callback){
-		session.sendRequest("sco-contents&sco-id="+folderID,null, xmlDoc => {
-			callback(xmlDoc.childNamed("scos").childrenNamed("sco").length)
-		})
-	}
-	createMeeting(){
-		// TODO: Need to insert template sco-id
-		var queryString = "sco-update&folder-id="+this.sectionID+"&type=meeting&name="+this.meetingName+"&source-sco-id="+session.settings.templateID+"&url-path="+this.meetingName.toLowerCase();
-		session.sendRequest(queryString,"sco@sco-id", id => {
-			if(id == undefined)
-				console.error("Error creating "+this.meetingName+" Check the template ID in the settings file");
-			else {
-				// Make this meeting public
-				var queryString = "permissions-update&acl-id="+id+"&principal-id=public-access&permission-id=view-hidden";
-				session.sendRequest(queryString,"status@code", status => {
-					if(status != "ok")
-						console.error("Error making "+this.meetingName+" public");
-					else {
-						// Make the teachers the host of the meeting
-						var queryString = "permissions-update&acl-id="+id+"&principal-id="+session.settings.adminID+"&permission-id=host";
-						session.sendRequest(queryString,"status@code", status => {
-							if(status != "ok")
-								console.error("Error making the teachers the host of "+this.meetingName+" Check the admin ID in the settings file");
-						})
+				
+				// parse the file and conform them to naming conventions
+				data = data.map( (d,i) => {
+					var cName = d["Course Name"].replace(" ","_").toUpperCase();
+					return {
+						course : cName,
+						section : ("000"+d["Section"]).slice(-3),
+						numMeet : d["Number of Meetings"] ? d["Number of Meetings"] : (numMeetLookUp[cName] ? numMeetLookUp[cName] : defaultNumOfMeetings)
 					}
+				}).filter( x => x.course )
+				
+				this.createSkeleton(data)
+
+				callback(null);
+			})
+		})
+	}
+
+	createSkeleton(data){
+
+		this.courses = {}
+
+		// creating objects for every single section
+		data.forEach( row => {
+
+			// check to see if we haven't come across this one already
+			if(!this.courses[row.course])
+				this.courses[row.course] = { ID:0, name:row.course, sections:{}}
+
+			// then create the section object (if it hasn't already been)
+			if(!this.courses[row.course].sections[row.section])
+				this.courses[row.course].sections[row.section] = { ID:0, name:row.section, numMeet:row.numMeet, meetings:{} }
+		})
+
+
+	}
+
+	createEverything(){
+
+		// for each course
+		asyn.mapLimit(this.courses,1, (course,courCallback) => {
+			// Create the course folder
+			new Folder(this.settings.coursesFolderID,course.name, (err,courseID) => {
+				if(err) { courCallback(err,0); return; }
+				// save the ID
+				this.courses[course.name].ID = courseID;
+
+				// For each section in this course
+				asyn.map(this.courses[course.name].sections,(section,sectCallback) => {
+					// Create the section folder
+					new Folder(this.courses[course.name].ID,section.name, (err,sectionID) => {
+						if(err) { sectCallback(err,0); return; }
+						// save the ID
+						this.courses[course.name].sections[section.name].ID = sectionID;
+
+						// And then do everything else
+						this.createMeetings(course.name,section,sectCallback)
+
+					})
+				}, (err, results) => {
+					// Done with this Course
+					// results is an array of arrays of errors
+					if(err)
+						console.error(err)
+					else
+						console.log("Done with "+course.name)
+					courCallback(err,results.length);
+				})
+			})
+		}, (err, results) => {
+			if(!err)
+				console.log("Success!")
+		})
+	}
+
+	createMeetings(courseName,section,sectCallback){
+		
+		// get list of previous meeting names
+		this.getContents(section.ID, (err,prevMeets) => {
+			if(err) { sectCallback("Couldn't get prev meeting names of "+courseName+"-"+section.name,0) }
+
+			// Create array of all needed meeting names
+			var meetings = [];
+			for(var i = 1; i <= section.numMeet; i++)
+				meetings.push(courseName.replace("_","") + "_G" + section.name + "_"+("000"+i).slice(-3))
+
+			// filter out the meetings that already exist
+			meetings = meetings.filter( x => prevMeets.indexOf(x) < 0)
+
+			// Creating each of the meetings
+			asyn.map(meetings, (meetingName,callback) => {
+
+				new Meeting(meetingName,section.ID, err => {
+					callback(err,err)
+				})
+
+			}, (err, results) => {
+				sectCallback(err,results)
+			})
+		})
+
+	}
+
+	getContents(folderID,callback){
+		main.sendRequest("sco-contents&sco-id="+folderID,null, (err,doc) => {
+			callback(err,doc.lastChild.children.map( x => x.firstChild.val))
+		})
+	}
+
+}
+
+class Folder{
+	constructor(parentFolderID, folderName, callback){
+		this.findFolderID(parentFolderID,folderName, (err,id) => {
+			if(err){ callback(err,0); return}
+			if(id){
+				callback(err,id)
+			} else {
+				this.createFolder(parentFolderID,folderName, (err, id) => {
+					callback(err,id)
 				})
 			}
 		})
 	}
+	findFolderID(parentFolderID,folderName,callback){
+		main.sendRequest("sco-contents&sco-id="+parentFolderID+"&filter-name="+folderName,"scos.sco@sco-id", callback)
+	}
+	createFolder(parentFolderID,folderName,callback){
+		main.sendRequest("sco-update&folder-id="+parentFolderID+"&type=folder&name="+folderName,"sco@sco-id", callback)
+	}
 }
 
+class Meeting{
+	constructor(meetingName,sectionID,callback){
+		this.meetingName = meetingName;
+		this.sectionID = sectionID;
+		this.callback = callback;
 
-// MAIN
+		// starting the chain of functions :)
+		this.makeMeeting()
+	}
+	makeMeeting(callback){
+		var queryString = "sco-update&folder-id="+this.sectionID+"&type=meeting&name="+this.meetingName+"&source-sco-id="+main.settings.templateID+"&url-path="+this.meetingName.toLowerCase();
+		main.sendRequest(queryString,"sco@sco-id", (err,id) => {
+			if(err) { this.callback(err); return }
+			if(!id) { this.callback("Couldn't create "+this.meetingName+" because something is invalid"); return }
 
-var session;
-cli.getSettings(settings => {
-	// There was an error with getting the settings
-	if(!settings) {return}
-	
-	// Creating an instance of Session, with the "settings" logs us in,
-	// and saves my session cookie
-	session = new Session(settings,() => {
-		
-		// There is probably a better way to do this, but works for now
-		fs.readFile(settings.csv,'utf8',(err,data) => {
-			if(err) { console.error(err) }
-			
-			// Parsing the CSV in one line!
-			var array = data.split('\r\n').slice(1,-1).map( x => x.split(','));
-			
-			// For each line in the CSV
-			for(var i = 0; i < array.length; i++){
-				
-				// minimal data validation
-				if(array[i][0] && array[i][1]){
-					
-					// calling my ginourmous Class constructor for each line in the CSV
-					var myClass = new Class(array[i][0],array[i][1],(array[i][2]?(array[i][2]*1):10));
-				
-				} else {
-					console.log("There was a problem with line "+(i+2)+" in the file");
-				}
-			}
+			this.id = id;
+			this.makePublic()
 		})
+	}
+	makePublic(id,callback){
+		var queryString = "permissions-update&acl-id="+this.id+"&principal-id=public-access&permission-id=view-hidden";
+		main.sendRequest(queryString,"status@code", (err, status) => {
+			if(err) { this.callback(err); return }
+			if(status != "ok") { this.callback("Error making "+this.meetingName+" public because something is invalid"); return}
+
+			this.setHost()
+		})
+	}
+	setHost(callback){
+		var queryString = "permissions-update&acl-id="+this.id+"&principal-id="+main.settings.adminID+"&permission-id=host";
+		main.sendRequest(queryString,"status@code", (err, status) => {
+			if(err) { this.callback(err); return }
+			if(status != "ok") { this.callback("Coudn't set the host of "+this.meetingName+" because something is invalid"); return }
+
+			this.callback(null)
+		})
+	}
+}
+
+let main;
+
+cli.getSettings(settings => {
+	if(!settings) {return;}
+	
+	main = new Main(settings, () => {
+
 	})
 })
